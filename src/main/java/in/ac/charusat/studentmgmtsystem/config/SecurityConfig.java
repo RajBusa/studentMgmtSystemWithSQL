6e985a55be6f125cf8945d285955532c81057876
RajBusa <20it0112charusat.edu.in>
2022-09-22 19:03:37 +0530

    bCryptPasswordEncoder complete

--- src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
+++ src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
@@ -1,24 +1,36 @@
 package in.ac.charusat.studentmgmtsystem.config;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
 @EnableWebSecurity
+//3.
+//@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+//    2.
 
     @Autowired
     DataSource dataSource;
 
+//    3.
+//    @Autowired
+//    UserServices userServices;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        1.
 //        auth.inMemoryAuthentication()
 //                .withUser("admin")
 //                .password("admin")
@@ -29,8 +41,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 //                .roles("USER");
 
 
-
-//             auth.jdbcAuthentication().dataSource(dataSource)
+//              2.
+             auth.jdbcAuthentication().dataSource(dataSource);
 //                     .withDefaultSchema()
 //                     .withUser(
 //                             User.withUsername("admin")
@@ -42,19 +54,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 //                                     .password("user")
 //                                     .roles("USER")
 //                     );
-
-
+//        3.
+//        auth.userDetailsService(userServices);
 
     }
-
+//    2.
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic();
         http.csrf().disable();
         http.headers().frameOptions().disable();
+//        http.authorizeRequests().antMatchers("/signup").permitAll();
+
+
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
